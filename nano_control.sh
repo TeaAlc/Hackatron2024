50aa65a1f6aa192157a6f7d1f3aad7c3f8db25c3
TeaAlc <86285862+TeaAlc@users.noreply.github.com>
2024-02-05 14:06:42 +0100

    Update nano_control.sh

--- nano_control.sh
+++ nano_control.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 export NANOLEAFS_IP="192.168.82.125"
+rnd=$RANDOM
+echo "RND: $rnd"
 
-if [[ $RANDOM -gt 16000 ]]; then
+if [[ rnd -gt 16000 ]]; then
  curl --location --request PUT "http://$NANOLEAFS_IP:16021/api/v1/$AUTH_TOKEN/state" --data "{\"hue\" : {\"value\":120}}"
 else
  curl --location --request PUT "http://$NANOLEAFS_IP:16021/api/v1/$AUTH_TOKEN/state" --data "{\"hue\" : {\"value\":7}}"
