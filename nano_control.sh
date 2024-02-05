ae7f3ebe9ffd2e53f20f5b47621a010d56b06076
TeaAlc <86285862+TeaAlc@users.noreply.github.com>
2024-02-05 14:46:11 +0100

    Update nano_control.sh

--- nano_control.sh
+++ nano_control.sh
@@ -2,15 +2,7 @@
 
 export NANOLEAFS_IP="192.168.82.125"
 
-if [ "$ITER" == "" ]; then
- export ITER=1
- echo "SET ITER: $ITER"
-fi
-
-export ITER=$((ITER+1))
-
-echo "ITER: $ITER"
-
+time=$(date +%s)
 if [ $(( ITER % 3 )) -eq 1 ]; then
  echo "green"
  curl -s --location --request PUT "http://$NANOLEAFS_IP:16021/api/v1/$AUTH_TOKEN/state" --data "{\"hue\" : {\"value\":120}}"
