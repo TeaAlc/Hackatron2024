dbdb6148d87939f8c1131e7b908648202743d671
TeaAlc <86285862+TeaAlc@users.noreply.github.com>
2024-02-05 14:44:42 +0100

    Update nano_control.sh

--- nano_control.sh
+++ nano_control.sh
@@ -13,11 +13,11 @@ echo "ITER: $ITER"
 
 if [ $(( ITER % 3 )) -eq 1 ]; then
  echo "green"
- curl --location --request PUT "http://$NANOLEAFS_IP:16021/api/v1/$AUTH_TOKEN/state" --data "{\"hue\" : {\"value\":120}}"
+ curl -s --location --request PUT "http://$NANOLEAFS_IP:16021/api/v1/$AUTH_TOKEN/state" --data "{\"hue\" : {\"value\":120}}"
 elif [ $(( ITER % 3 )) -eq 2 ]; then
  echo "yellow"
- curl --location --request PUT "http://$NANOLEAFS_IP:16021/api/v1/$AUTH_TOKEN/state" --data "{\"hue\" : {\"value\":39}}"
+ curl -s --location --request PUT "http://$NANOLEAFS_IP:16021/api/v1/$AUTH_TOKEN/state" --data "{\"hue\" : {\"value\":39}}"
 else
  echo "red"
- curl --location --request PUT "http://$NANOLEAFS_IP:16021/api/v1/$AUTH_TOKEN/state" --data "{\"hue\" : {\"value\":7}}"
+ curl -s --location --request PUT "http://$NANOLEAFS_IP:16021/api/v1/$AUTH_TOKEN/state" --data "{\"hue\" : {\"value\":7}}"
 fi
